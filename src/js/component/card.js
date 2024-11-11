59d6a70ded93709827165b2cae4758ed7ce26b74
Anahi Vera <168015268+AnahiVera@users.noreply.github.com>
2024-11-11 16:23:56 +0000

    component

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -16,7 +16,7 @@ export const Card = ({ item, type }) => {   /* item puede ser character, vehicle
 			<div className="card-body">
 				<h5 className="card-title">{item?.name}</h5>
 
-				<button onClick={() => actions.addFavorites(item)} className="btn btn-info">
+				<button onClick={() => actions.addFavorites(item)} className="btn btn-info m-1">
 					<i className={isFavorite ? "fa-solid fa-heart" : "fa-regular fa-heart"}></i>
 				</button>
 				{
