e661a644f02ec569d51740e79da712b6efbcf193
Anahi Vera <168015268+AnahiVera@users.noreply.github.com>
2024-11-19 18:00:59 +0000

    dropdown style

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -12,14 +12,12 @@ export const Navbar = () => {
 					<span>Star Wars</span>
 				</Link>
 				<div className="btn-group">
-					<button type="button" className="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown"
+					<button type="button" className="btn btn-warning dropdown-toggle" data-bs-toggle="dropdown" data-bs-auto-close="outside"
 						aria-expanded="false">Favorites ({store?.favorites?.length})</button>
-					<ul className="dropdown-menu dropdown-menu-end">
-
-
+					<ul className="dropdown-menu dropdown-menu-end text-warning bg-dark">
 						{store?.favorites && store?.favorites.length > 0 ? (
 							store?.favorites?.map((item, index) => (
-								<li key={index} className="dropdown-item d-flex justify-content-between align-items-center">
+								<li key={index} className="dropdown-item d-flex justify-content-between align-items-center text-warning bg-dark">
 									<Link
 										to={
 											item.type === "people"
@@ -30,19 +28,19 @@ export const Navbar = () => {
 														? `/detailsPlanets/${item.uid}`
 														: "/not-found" // Ruta por defecto si no coincide con ningún tipo
 										}
-										className="text-decoration-none text-dark"
+										className="text-decoration-none text-warning bg-dark"
 										onClick={() => console.log(`Navigating to: ${item.type}, UID: ${item.uid}`)}>
 										{item.name}
 									</Link>
 
-									<button className="btn btn-sm btn-danger ms-2">
+									<button className="btn btn-sm btn-warning ms-2">
 										<i className="fas fa-trash-alt" onClick={() => actions.deleteFavorites(item)}></i>
 									</button>
 								</li>))
 
 						) : (
-							<li className="dropdown-item d-flex justify-content-between align-items-center">
-								<p>No favorites added</p></li>
+							<li className="dropdown-item d-flex justify-content-between align-items-center bg-dark">
+								<p className="text-warning bg-dark">No favorites added</p></li>
 						)}
 					</ul>
 				</div>
